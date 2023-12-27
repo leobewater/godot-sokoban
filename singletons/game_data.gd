af3e55fa5cafc58da8a5759a73c4ad20b9924fb7
leobewater <mmbee888@gmail.com>
2023-12-27 13:05:41 -0500

    load level game data from json file

--- singletons/game_data.gd
+++ singletons/game_data.gd
@@ -1,11 +1,23 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
+const LEVEL_DATA_PATH: String = "res://data/level_data.json"
+const TILE_SIZE: int = 32
+
+
+var _level_data: Dictionary = {}
+
+
 func _ready():
-	pass # Replace with function body.
+	load_level_data()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func load_level_data() -> void:
+	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
+	_level_data = JSON.parse_string(file.get_as_text())
 	pass
+
+
+func get_data_for_level(level_number: String) -> Dictionary:
+	# TODO: check level exists
+	return _level_data[level_number]
